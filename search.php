ad105450abd0719d62e65773d4f706a5143e2019
JohnnyElmer <45784589+JohnnyElmer@users.noreply.github.com>
2018-12-19 09:23:52 +0100

    Update search.php

--- search.php
+++ search.php
@@ -6,12 +6,25 @@
  if(mysqli_errno($conn)) {
   die("<p>MySQL error:</p>\n<p>" . mysqli_error($conn) . "</p>\n</body>\n</html>\n");
  }
+	
+$SetID =  $_GET['set'];
+	
  // Fråga efter delarna hämtar ut dem
- $contents = mysqli_query($conn, "SELECT inventory.Quantity, inventory.ItemTypeID, inventory.ItemID, inventory.ColorID, colors.Colorname, parts.Partname FROM inventory, parts, colors WHERE inventory.SetID='375-2' AND inventory.ItemTypeID='P' AND inventory.ItemID=parts.PartID AND inventory.ColorID=colors.ColorID");
+ $contents = mysqli_query($conn, "SELECT inventory.Quantity, inventory.ItemTypeID, inventory.ItemID,
+ 				inventory.ColorID, colors.Colorname, parts.Partname, sets.Setname, 
+				FROM inventory, parts, colors 
+				WHERE inventory.SetID='$SetID'
+				OR sets.Setname= '$SetID'
+				AND inventory.ItemTypeID='P' 
+				AND inventory.ItemID=parts.PartID 
+				AND inventory.ColorID=colors.ColorID");
+	
  if(mysqli_num_rows($contents) == 0) {
   print("<p>No parts in inventory for this set.</p>\n");
- } else {
-  // Skriver ut tabell 
+ } 
+else {
+ 
+// Skriver ut tabell 
   print("<p>Parts in set:</p>");
   print("<table>\n<tr>");
   print("<th>Quantity</th>");
@@ -20,19 +33,24 @@
   print("<th>Color</th>");
   print("<th>Part name</th>");
   print "</tr>\n";
-  // While funktionen som skriver ut i rätt ordning
+  
+// While funktionen som skriver ut i rätt ordning
   while($row = mysqli_fetch_array($contents)) {
    print("<tr>");
    $Quantity = $row['Quantity'];
    print("<td>$Quantity</td>");
-   // Bestäm filnamnet för en bild som är 80x60 pixlar, helst i JPG-format.
+  
+// Bestäm filnamnet för en bild som är 80x60 pixlar, helst i JPG-format.
    $prefix = "http://www.itn.liu.se/~stegu76/img.bricklink.com/";
    $ItemID = $row['ItemID'];
    $ColorID = $row['ColorID'];
-   // Fråga databasen för att se vilka filer som finns !
+   
+// Fråga databasen för att se vilka filer som finns !
    $imagesearch = mysqli_query($conn, "SELECT * FROM images WHERE ItemTypeID='P' AND ItemID='$ItemID' AND ColorID=$ColorID");
-   // Frågan returnera en rad 
+  
+// Frågan returnera en rad 
    $imageinfo = mysqli_fetch_array($imagesearch);
+  
    if($imageinfo['has_jpg']) { // Använd JPG om den finns
 	 $filename = "P/$ColorID/$ItemID.jpg";
    } else if($imageinfo['has_gif']) { // Använd GIF om JPG inte tillgägligt 
@@ -40,6 +58,7 @@
    } else { // Om ingen format finns skriv ut text 
 	 $filename = "noimage_small.png";
    }
+  
    print("<td>$filename</td>");
    print("<td><img src=\"$prefix$filename\" alt=\"Part $ItemID\"/></td>");
    $Colorname = $row['Colorname'];
