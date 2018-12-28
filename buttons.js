8cdda06c93125f544e9bd3d256fbf88459bdd782
JohnnyElmer <45784589+JohnnyElmer@users.noreply.github.com>
2018-12-28 10:34:52 +0100

    Add files via upload

---
+++ buttons.js
@@ -0,0 +1,9 @@
+function skaparna(){
+confirm("Testing skaparna");
+document.getElementById("Skaparna").innerHTML = txt;
+}
+
+function sökhjälp(){
+confirm("Skriv in ett satsnamn eller satsID och tryck sedan enter!");
+document.getElementById("Sökhjälp").innerHTML = txt;
+}
