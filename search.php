3c0f6dc36406d0c337afb15dd5b38a325fa58775
PiratarrRn <33228484+PiratarrRn@users.noreply.github.com>
2019-01-11 16:01:41 +0100

    nästa- och föregåendeknappar

--- search.php
+++ search.php
@@ -1,6 +1,15 @@
 
 <?php
 	
+$offset= $_GET['offset'];
+$lim=20;
+$totalt=0;
+
+if($offset<0)
+{
+	
+}
+	
 // Anslut til LEGOdatabasen 
  $conn = mysqli_connect("mysql.itn.liu.se","lego","","lego");
  if(mysqli_errno($conn)) {
@@ -8,8 +17,18 @@
  }
 	
 $SetID =  $_GET['set'];
+$originalsokning=$SetID;
 if($SetID!="")
 {
+	//rekne_query är fulkodning och gör sökningarna sega, men det funkar. Om möjligt, se om det går att räkna antalet element innan LIMIT körs.
+	$rekne_query= "SELECT COUNT(*) AS totalt  
+			FROM (((parts INNER JOIN inventory ON parts.PartID=inventory.ItemID) 
+			INNER JOIN colors ON colors.ColorID=inventory.ColorID) 
+			INNER JOIN sets 
+			ON sets.SetID=inventory.SetID) 
+			WHERE (inventory.SetID LIKE '%".$SetID."%' OR sets.Setname LIKE '%".$SetID."%') 
+			AND inventory.ItemTypeID='P'";
+	
 	$query= "SELECT inventory.Quantity, inventory.ItemTypeID, inventory.ItemID, inventory.SetID, colors.ColorID,colors.Colorname, parts.Partname, sets.Setname 
 			FROM (((parts INNER JOIN inventory ON parts.PartID=inventory.ItemID) 
 			INNER JOIN colors ON colors.ColorID=inventory.ColorID) 
@@ -17,11 +36,18 @@ if($SetID!="")
 			ON sets.SetID=inventory.SetID) 
 			WHERE (inventory.SetID LIKE '%".$SetID."%' OR sets.Setname LIKE '%".$SetID."%') 
 			AND inventory.ItemTypeID='P' 
-			LIMIT 20";
+			LIMIT ".$offset.",".$lim;
+			
 	 
-	 $contents = mysqli_query($conn, $query);
-		
-	echo "antal rader ".mysqli_num_rows($contents);
+	$contents = mysqli_query($conn, $query);	 
+	$rekne_resultat=mysqli_query($conn, $rekne_query);
+	
+	  while($row = mysqli_fetch_array($rekne_resultat)) //utskrift av totala resultat
+		{
+			$totalt=$row['totalt'];
+			echo "Totalt ".$totalt." resultat hittades.";
+		}
+	
 	 if(mysqli_num_rows($contents) == 0)
 	 {
 	  print("<p>No parts in inventory for this set.</p>\n");
@@ -85,7 +111,7 @@ if($SetID!="")
 			   $Colorname = $row['Colorname'];
 			   $Partname = $row['Partname'];
 			   print("<td>$Colorname</td>");
-			   print("<td><a href='http://www.student.itn.liu.se/~linsv482/projekt/hem.php?set=".$SetID."'>".$SetID."</a></td>");
+			   print("<td><a href='http://www.student.itn.liu.se/~linsv482/projekt/hem.php?set=".$SetID."&offset=0'>".$SetID."</a></td>");
 			   print("<td>$SetName</td>");
 			   print("</tr>\n");
 		  }
@@ -93,10 +119,20 @@ if($SetID!="")
 	 }
 	 
 }
- 
- 
- 
- 
+
+$linkprevious="http://www.student.itn.liu.se/~linsv482/projekt/hem.php?set=".$originalsokning."&offset=".($offset-20);
+$linknext="http://www.student.itn.liu.se/~linsv482/projekt/hem.php?set=".$originalsokning."&offset=".($offset+20);
+?>
+
+
+<button onclick="window.location.href = '<?PHP echo $linkprevious; ?>';" <?PHP if($offset<=0) echo "disabled" ?>>Föregående</button>
+<button onclick="window.location.href = '<?PHP echo $linknext; ?>';" <?PHP if($totalt-$lim<=$offset) echo "disabled" ?>>Nästa</button>
+
+
+
+ <?PHP
+// print("<a href='http://www.student.itn.liu.se/~linsv482/projekt/hem.php?set=".$originalsokning."&offset=".($offset-20)."'>Föregående</a>");
+ //print("<a href='http://www.student.itn.liu.se/~linsv482/projekt/hem.php?set=".$originalsokning."&offset=".($offset+20)."'>Nästa</a>");
  
 //header("Location: hem.php",true,301);
 //exit();	
