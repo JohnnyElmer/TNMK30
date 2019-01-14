8590eba5eca604d15833158b2af7c8869be58bd6
Shamoun095 <45786632+Shamoun095@users.noreply.github.com>
2019-01-14 16:01:59 +0100

    länkade till sidfot.php

---
+++ skaparna.php
@@ -0,0 +1,41 @@
+<!doctype html>
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>LEGO-Databasen</title>
+	<link href="Lego.css" media="screen" rel="stylesheet" type="text/css"/>
+	<script src="buttons.js"></script>
+</head>
+<body>
+<div id="container">
+<a href="hem.php">
+	<div id="logo">
+		<h1>LEGO-Databasen</h1>
+	</div>
+</a>
+</div>
+
+<div id="searchbar">
+	<?php include 'formuler.php'; ?>
+</div>
+<div id="tabelldiv">
+	<p>Lego är det bästa jag vet.</p>
+	<div id="tabellinneholl">
+		<p>Resultattabell</p>
+		<?php
+		include 'search.php';
+		
+		?>
+	</div>
+</div>
+
+<div id="sidfot">
+	
+	<?php include 'sidfot.php'; ?>
+	
+	
+	
+</div>
+</body>
+</html>
