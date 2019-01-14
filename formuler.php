546379c3323fbb914b911e56d25fd6ba682f4c50
PiratarrRn <33228484+PiratarrRn@users.noreply.github.com>
2019-01-14 15:37:38 +0100

    dynamiskt formulär

---
+++ formuler.php
@@ -0,0 +1,6 @@
+	<form  method="GET">
+		<input type="text" name="set" placeholder="Sök efter legosatser..."/>
+		 Antal resultat per sida: <input type="number" name="antal" min="1" value="5"/>
+		 Visa de resultat som kommer efter resultat nummer (offset): <input type="number" name="offset" min="0" value="0"/>
+		<input type="submit" value="Submit">
+	</form>
