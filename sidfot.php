81c79faffeecf593d20fe5818d18b2aff80953f9
Shamoun095 <45786632+Shamoun095@users.noreply.github.com>
2019-01-14 16:02:58 +0100

    lade till sidfot.php

---
+++ sidfot.php
@@ -0,0 +1,14 @@
+
+	
+	
+	<a href ="skaparna.php" style="display:block;">
+	<div class="cirkeldiv" >
+		<p id="Skaparna"><br>Skaparna</p> 
+	</div>
+	</a>
+	
+	<a href="help.php" style="display:block;">
+	<div class="cirkeldiv" >
+		<p id="Sökhjälp"><br>try</p>
+	</div>
+	</a>
