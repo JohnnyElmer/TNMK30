a0c072681258a0e04c758bf9980f66ae78a60b29
PiratarrRn <33228484+PiratarrRn@users.noreply.github.com>
2019-01-14 14:18:46 +0100

    uppdaterade search.php

--- search.php
+++ search.php
@@ -2,7 +2,7 @@
 <?php
 	
 $offset= $_GET['offset'];
-$lim=20;
+$lim=5;
 $totalt=0;
 
 if($offset<0)
@@ -19,36 +19,37 @@ if($offset<0)
 $SetID =  $_GET['set'];
 $originalsokning=$SetID;
 if($SetID!="")
-{
-	//rekne_query är fulkodning och gör sökningarna sega, men det funkar. Om möjligt, se om det går att räkna antalet element innan LIMIT körs.
+{	/*
 	$rekne_query= "SELECT COUNT(*) AS totalt  
 			FROM (((parts INNER JOIN inventory ON parts.PartID=inventory.ItemID) 
 			INNER JOIN colors ON colors.ColorID=inventory.ColorID) 
 			INNER JOIN sets 
 			ON sets.SetID=inventory.SetID) 
 			WHERE (inventory.SetID LIKE '%".$SetID."%' OR sets.Setname LIKE '%".$SetID."%') 
-			AND inventory.ItemTypeID='P'";
-	
-	$query= "SELECT inventory.Quantity, inventory.ItemTypeID, inventory.ItemID, inventory.SetID, colors.ColorID,colors.Colorname, parts.Partname, sets.Setname 
+			AND inventory.ItemTypeID='P'";	
+
+*/
+	$query= "SELECT inventory.Quantity, inventory.ItemTypeID, inventory.ItemID, inventory.SetID, colors.ColorID,colors.Colorname, parts.Partname, sets.Setname  
 			FROM (((parts INNER JOIN inventory ON parts.PartID=inventory.ItemID) 
 			INNER JOIN colors ON colors.ColorID=inventory.ColorID) 
 			INNER JOIN sets 
 			ON sets.SetID=inventory.SetID) 
 			WHERE (inventory.SetID LIKE '%".$SetID."%' OR sets.Setname LIKE '%".$SetID."%') 
 			AND inventory.ItemTypeID='P' 
-			LIMIT ".$offset.",".$lim;
+			LIMIT ".$offset.",".($lim+1);
 			
 	 
-	$contents = mysqli_query($conn, $query);	 
-	$rekne_resultat=mysqli_query($conn, $rekne_query);
+	$contents = mysqli_query($conn, $query);	
+	//$rekne_resultat=mysqli_query($conn, $rekne_query);
 	
-	  while($row = mysqli_fetch_array($rekne_resultat)) //utskrift av totala resultat
-		{
-			$totalt=$row['totalt'];
-			echo "Totalt ".$totalt." resultat hittades.";
-		}
+
+$totalt=mysqli_num_rows($contents);
+
+	echo "Visar resultat nummer ".$offset." till ".($offset+$totalt).".";
+
+
 	
-	 if(mysqli_num_rows($contents) == 0)
+	 if($totalt == 0)
 	 {
 	  print("<p>No parts in inventory for this set.</p>\n");
 	 } 
@@ -67,6 +68,7 @@ if($SetID!="")
 		  print("<th>Set Name</th>");
 		  print "</tr>\n";
 		  
+		
 		// While funktionen som skriver ut i rätt ordning
 		  while($row = mysqli_fetch_array($contents))
 		  {
@@ -126,7 +128,7 @@ $linknext="http://www.student.itn.liu.se/~linsv482/projekt/hem.php?set=".$origin
 
 
 <button onclick="window.location.href = '<?PHP echo $linkprevious; ?>';" <?PHP if($offset<=0) echo "disabled" ?>>Föregående</button>
-<button onclick="window.location.href = '<?PHP echo $linknext; ?>';" <?PHP if($totalt-$lim<=$offset) echo "disabled" ?>>Nästa</button>
+<button onclick="window.location.href = '<?PHP echo $linknext; ?>';" <?PHP if($totalt<$lim) echo "disabled" ?>>Nästa</button>
 
 
 
