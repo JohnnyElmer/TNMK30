fb1f07d086f916add8de8907f8e4ae0bcf15a0ea
Shamoun095 <45786632+Shamoun095@users.noreply.github.com>
2019-01-18 16:15:47 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,38 @@
 <html>
 	<?php include 'header_search.php'; ?>
 	
-<div id="tabelldiv">
-	<p>Lego är det bästa jag vet.</p>
+
+
+<div id="pics">
+
+<div class="slideshow-container">
+
+<div class="mySlides fade">
+  <div class="numbertext"></div>
+  <img src="img/crain.png" style="width:100%">
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext"></div>
+  <img src="img/batman.png" style="width:100%">
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext"></div>
+  <img src="img/lego.png" style="width:100%">
+</div>
+
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+</div>
+</div>
+
+</div>
 </div>
 
 <div id="sidfot">
