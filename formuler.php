57dc429e05def04e7b72a1b9022527c7c261ed29
PiratarrRn <33228484+PiratarrRn@users.noreply.github.com>
2019-01-18 14:46:34 +0100

    omdirigerar till sets.php

--- formuler.php
+++ formuler.php
@@ -1,4 +1,4 @@
-	<form  method="GET">
+	<form  method="GET" action="sets.php">
 		<input type="text" name="set" placeholder="Sök efter legosatser..."/>
 		 Antal resultat per sida: <input type="number" name="antal" min="1" value="5"/>
 		 Visa de resultat som kommer efter resultat nummer (offset): <input type="number" name="offset" min="0" value="0"/>
