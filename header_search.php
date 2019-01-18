344baffe137d63c34584d759b41660e9d7adf1b8
JohnnyElmer <45784589+JohnnyElmer@users.noreply.github.com>
2019-01-18 14:46:43 +0100

    Add files via upload

---
+++ header_search.php
@@ -0,0 +1,18 @@
+<head>
+	<meta charset="utf-8">
+	<title>LEGO-Databasen</title>
+	<link href="Lego.css" media="screen" rel="stylesheet" type="text/css"/>
+</head>
+
+<body>
+<div id="container">
+<a href="index.php">
+	<div id="logo">
+		<h1>LEGO-Databasen</h1>
+	</div>
+</a>
+</div>
+
+<div id="searchbar">
+	<?php include 'formuler.php'; ?>
+</div>
