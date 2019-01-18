684e07a31ebc3ef66d116248195e4ee2f6e5eef3
JohnnyElmer <45784589+JohnnyElmer@users.noreply.github.com>
2019-01-18 14:51:40 +0100

    header change

--- help.php
+++ help.php
@@ -1,25 +1,8 @@
 <!doctype html>
 
 <html>
-<head>
-	<meta charset="utf-8">
-	<title>LEGO-Databasen</title>
-	<link href="Lego.css" media="screen" rel="stylesheet" type="text/css"/>
+	<?php include 'header_search.php'; ?>
 	
-</head>
-<body>
-<div id="container">
-<a href="index.php">
-	<div id="logo">
-		<h1>LEGO-Databasen</h1>
-	</div>
-</a>
-</div>
-
-<div id="searchbar">
-	<?php include 'formuler.php'; ?>
-	
-</div>
 <div> <a href="search.php"></a>
 </div >
 
