3199469363e1555ad1195dca5c453d66737f1dc2
JohnnyElmer <45784589+JohnnyElmer@users.noreply.github.com>
2019-01-18 14:51:59 +0100

    header change

--- index.php
+++ index.php
@@ -1,24 +1,8 @@
 <!doctype html>
 
 <html>
-<head>
-	<meta charset="utf-8">
-	<title>LEGO-Databasen</title>
-	<link href="Lego.css" media="screen" rel="stylesheet" type="text/css"/>
-	<script src="buttons.js"></script>
-</head>
-<body>
-<div id="container">
-<a href="index.php">
-	<div id="logo">
-		<h1>LEGO-Databasen</h1>
-	</div>
-</a>
-</div>
-
-<div id="searchbar">
-	<?php include 'formuler.php'; ?>
-</div>
+	<?php include 'header_search.php'; ?>
+	
 <div id="tabelldiv">
 	<p>Lego är det bästa jag vet.</p>
 </div>
