f46c75c5c530f201ee7e63a738bce7a661605595
JohnnyElmer <45784589+JohnnyElmer@users.noreply.github.com>
2019-01-19 15:24:26 +0100

    commented

--- formuler.php
+++ formuler.php
@@ -1,10 +1,14 @@
-	<form  method="GET" action="sets.php"> <!-- information gathered is sent to sets.php -->
+<!-- information gathered is sent to sets.php, with the information being visible in the generated link -->	
+<form  method="GET" action="sets.php"> 
 		
 		<!-- the "boxes" for entering your search and results per page -->
+		<!-- $_GET['set'] "saves" what the user just searched for,
+				in case they want to adjust the number of results shown per page -->
 		<?PHP echo "<input type='text' name='set' placeholder='Sök efter legosatser...' value='".$_GET['set']."' />"; ?>
 		 Antal resultat per sida: <input type="number" name="antal" min="1" value="5"/>
 		
-		<!-- sets a hidden variable used in search_sets and search_parts  -->
+		<!-- sets a hidden variable used in search_sets and search_parts, the value is shown in the generated link 
+			and is changed when browsing between result pages  -->
 		<input type="hidden" name="offset" value="0"/>
 		<input type="submit" value="Submit">
 	</form>
