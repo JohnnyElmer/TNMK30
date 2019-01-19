6a26616076f2488d09679e1807a260cb88c94e5d
JohnnyElmer <45784589+JohnnyElmer@users.noreply.github.com>
2019-01-19 13:34:39 +0100

    commented

--- sets.php
+++ sets.php
@@ -1,20 +1,22 @@
 <!doctype html>
 
 <html>
+	<!-- includes a file containing header and searchbar -->
 	<?php include 'header_search.php'; ?>
 	
-<div id="tabelldiv">
+<div id="tabelldiv"> <!-- div to hold a table -->
 	<p>Lego är det bästa jag vet.</p>
-	<div id="tabellinneholl">
-		<p>Resultattabell</p>
+	<div id="tabellinneholl"> <!-- link for css -->
+		<p>Resultattabell</p> 
+		<!-- includes a file that sends a question to the lego-database and creates a table with the answer -->
 		<?php
 		include 'search_sets.php';
-		
 		?>
 	</div>
 </div>
 
-<div id="sidfot">	
+<div id="sidfot">
+	<!-- includes a file containing the footer -->
 	<?PHP include 'sidfot.php'; ?>		
 </div>
 </body>
