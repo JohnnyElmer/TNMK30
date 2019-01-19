4cfab545eee756e3ca33c7a6741b9c626a73b75e
JohnnyElmer <45784589+JohnnyElmer@users.noreply.github.com>
2019-01-19 13:16:50 +0100

    commented

--- sidfot.php
+++ sidfot.php
@@ -1,13 +1,13 @@
 
 	
 	
-	<a href ="skaparna.php" >
+	<a href ="skaparna.php" > <!-- sends to page that has info about creators-->
 	<div class="cirkeldiv" >
 		<p id="Skaparna"><br>Skaparna</p> 
 	</div>
 	</a>
 	
-	<a href="help.php">
+	<a href="help.php"> <!-- sends to page that explains the search function -->
 	<div class="cirkeldiv" >
 		<p id="Sökhjälp"><br>try</p>
 	</div>
