177293cae289628c0875017afa77119b81004274
JohnnyElmer <45784589+JohnnyElmer@users.noreply.github.com>
2019-01-19 11:14:14 +0100

    Update help.php

--- help.php
+++ help.php
@@ -10,11 +10,13 @@
 	<h2>Såhär funkar sökfunktionen</h2>
 		<p>Klicka på sökrutan och skriv in antingen ett namn på en sats (bokstäver)
 		eller satsid (siffror). Du behöver inte vara orolig om du inte vet det fulla namnet
-		eller id:t, du kan söka på delar av dessa.</p>
+		eller id:t, du kan söka på delar av dessa. Välj sedan i ruta nummer två antalet resultat du
+		vill se per sida.</p>
 		
-		<p>När du skrivit in det du vill söka på klickar du på enter på ditt keyboard och sökningen utförs, 
-		resultatet kommer komma upp i formen av en tabell som går att skrolla genom. Klicka sedan på den sats
-		du vill se innehållet av!</p>
+		<p>När du skrivit in det du vill söka på och valt antal resultat 
+		per sida klickar du på enter på ditt keyboard och sökningen utförs, 
+		resultatet kommer komma upp i formen av en tabell som går att skrolla genom. 
+		Klicka sedan på den sats du vill se innehållet av!</p>
 		
 		<p>Om du någon gång känner dig borttappad, klicka på "loggan" längst upp på sidan för att komma tillbaka
 		till startsidan, eller klicka på sökhjälp för att komma tillbaka till dessa instruktioner.</p>
